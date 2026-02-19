77c0a33775cd5b34347c3e266b6f4203a9a3c336
southen_ <alabanaomi2020@icloud.com>
2026-02-19 12:22:46 +0100

    fix: correctly close Prophecy view nesting structure

--- src/app/page.js
+++ src/app/page.js
@@ -1835,7 +1835,8 @@ export default function Home() {
                     ))}
                   </div>
                 </div>
-                </div>
+              )}
+            </div>
           )}
 
 
